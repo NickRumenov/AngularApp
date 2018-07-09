df7e6113806b03a4eef0727c51f3a398ec850f86
Nikolay Rumenov <nikolay.rumenov@axsmarine.com>
2018-07-09 10:52:50 +0300

    Added Heroes routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -7,6 +7,7 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
